c93e5d1f96e6baee5e38bad7c3b085108bd8f723
alicemayle <alicemr273@gmail.com>
2022-08-08 17:38:02 -0500

    se agrega appbar y footer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
+import AppBarMenu from './components/AppBarMenu';
+import Footer from './components/Footer';
 import Home from './containers/Home';
 
 class App extends Component {
   render() {
     return (
-      <div className="wrapper">
+      <div className='init'>
+        <AppBarMenu />
         <Home />
+        <Footer />
       </div>
     );
   }
