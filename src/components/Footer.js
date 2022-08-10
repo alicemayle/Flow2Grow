d7b72d56e71b6a7bfe11ddbfecd4a40c27f0037a
alicemayle <alicemr273@gmail.com>
2022-08-10 09:17:56 -0500

    cambio iconos footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,40 +2,32 @@ import { IconButton } from '@mui/material';
 import React, { Component } from 'react';
 import PhoneIcon from '@mui/icons-material/Phone';
 import EmailIcon from '@mui/icons-material/Email';
-import InstagramIcon from '@mui/icons-material/Instagram';
-import FacebookIcon from '@mui/icons-material/Facebook';
-import TwitterIcon from '@mui/icons-material/Twitter';
+import WhatsAppIcon from '@mui/icons-material/WhatsApp';
 
 class Footer extends Component {
     render() {
     return (
         <div className='footer'>
-            <div className='boxIconsFooter' style={{textAlign: 'left'}}>
+            <div className='box-icons-footer' style={{textAlign: 'left'}}>
                 <span style={{fontSize: 'small'}}>
                     ©2020 by Flow2Grow.
                 </span>
             </div>
-            <div className='boxIconsFooter'  style={{textAlign: 'center'}}>
-                <span>Privacy Policy</span>
-                <div>
+            <div className='box-icons-footer'  style={{textAlign: 'center', flexDirection: 'column' }}>
+                <span>Privacy Policy<br></br></span>
+                <span>Terms & Conditions</span>
+            </div>
+            <div className='box-icons-footer'  style={{textAlign: 'right'}}>
                 <IconButton color="info" aria-label="contact phone">
                     <PhoneIcon />
                 </IconButton>
-                <IconButton color="info" aria-label="contact phone">
+                <IconButton color="info" aria-label="contact whatsapp">
+                    <WhatsAppIcon />
+                </IconButton>
+                <IconButton color="info" aria-label="contact email">
                     <EmailIcon />
                 </IconButton>
-                </div>
-            </div>
-            <div className='boxIconsFooter'  style={{textAlign: 'right'}}>
-                <IconButton color="info" aria-label="contact twitter">
-                    <TwitterIcon />
-                </IconButton>
-                <IconButton color="info" aria-label="contact facebook">
-                    <FacebookIcon />
-                </IconButton>
-                <IconButton color="info" aria-label="contact instagram">
-                    <InstagramIcon />
-                </IconButton>
+
             </div>
       </div>
     );
